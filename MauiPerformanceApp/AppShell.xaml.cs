c56d3a0f34a1724d93cf7cf6c0200ea8760a08b2
Blumenzupfer <sebastianheyde@t-online.de>
2024-01-08 18:35:39 +0100

    Add project files.

---
+++ MauiPerformanceApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiPerformanceApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MauiPerformanceApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiPerformanceApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
